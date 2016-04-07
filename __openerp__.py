0c812136d16578516ef25f765411daf919e16666
remixazka <remixazka@192.168.0.12>
2016-04-07 09:30:47 +0200

    Commit inicial, y creacion de archivos basicos del tema. Modificado openep__, se le agregaron las principales propiedades de un tema: 	name 	description 	version 	author 	data 	category 	depends

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+  'name':'Tema Personal',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Beatriz Hernandez',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_less'],
+}
